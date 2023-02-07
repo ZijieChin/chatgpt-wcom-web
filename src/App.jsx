53eee1ef65246598a66ff82cfe01a983d8579795
ZijieChin <zijieq95@gmail.com>
2023-02-07 20:49:07 +0800

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import './App.css'
 import Conversation from './Conversation'
 
@@ -32,6 +32,24 @@ function App() {
     },
   ])
 
+  const chatBoxBottom = useRef(null)
+
+  const onSubmitClick = () => {
+    let question = { user: 'user', content: userInput }
+    setConversations([...conversations, question])
+    setUserInput('')
+  }
+
+  const scrollToBottom = () => {
+    if (chatBoxBottom.current && chatBoxBottom) {
+      chatBoxBottom.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [conversations])
+
   return (
     <div className="App p-0 max-h-full">
       <div className="flex flex-col h-full">
@@ -41,19 +59,25 @@ function App() {
         </div>
         <div className="basis-10/12 flex flex-col mt-10 mb-16 bg-neutral-50">
           <Conversation content={conversations} />
+          <div className="bottom" ref={chatBoxBottom}></div>
           <div className="basis-1/12 flex flex-row bg-red-500 items-center p-2 fixed bottom-0 w-full">
             <div className="basis-4/5 flex items-center">
-              <textarea
+              <input
                 className="rounded-md w-full h-9 outline-none border-slate-50	border-2 bg-neutral-50"
                 onChange={(e) => {
                   setUserInput(e.target.value)
-                  console.log(userInput)
                 }}
                 value={userInput}
+                onKeyUp={(e) => {
+                  if (e.key === 'Enter') onSubmitClick()
+                }}
               />
             </div>
             <div className="basis-1/5 flex items-center">
-              <button className="rounded-md w-full h-9 bg-blue-400 ml-1 outline-none font-semibold text-gray-50">
+              <button
+                onClick={onSubmitClick}
+                className="rounded-md w-full h-9 bg-blue-400 ml-1 outline-none font-semibold text-gray-50"
+              >
                 发 送
               </button>
             </div>
--- src/Conversation.jsx
+++ src/Conversation.jsx
@@ -6,23 +6,23 @@ function Conversation(props) {
     <div className="flex-row w-full">
       {props.content.map((item, index) =>
         item.user === 'user' ? (
-          <div className="w-full flex flex-row-reverse m-2">
-            <div className="basis-1/6 flex items-end justify-center">
+          <div className="w-full flex flex-row-reverse mt-2 mb-2">
+            <div className="basis-1/6 flex justify-center">
 							<img src={user} alt="" className="h-10 w-10" />
 						</div>
             <div className="basis-4/6">
-							<div className="bg-gray-200 p-2 rounded-tl-lg  rounded-tr-lg rounded-bl-lg">
+							<div className="bg-gray-200 p-2 rounded-tl-xl  rounded-br-xl rounded-bl-xl shadow-md -mr-1">
 								{item.content}
 							</div>
 						</div>
           </div>
         ) : (
-          <div className="w-full flex flex-row">
-            <div className="basis-1/6 flex items-end justify-center">
+          <div className="w-full flex flex-row mt-2 mb-2">
+            <div className="basis-1/6 flex justify-center">
 							<img src={chatgpt} alt="" className="h-10 w-10" />
 					  </div>
             <div className="basis-4/6">
-							<div className="bg-red-200 p-2 rounded-tl-lg  rounded-tr-lg rounded-br-lg">
+							<div className="bg-red-200 p-2 rounded-br-xl  rounded-tr-xl rounded-bl-xl shadow-md -ml-1">
 								{item.content}
 							</div>
 						</div>
