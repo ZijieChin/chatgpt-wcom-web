61838c152ff1b07a98f2b8017e981fe051e53eed
qzj995@outlook.com <qzj995@outlook.com>
2023-02-08 16:15:49 +0800

    update ui

--- src/Conversation.jsx
+++ src/Conversation.jsx
@@ -1,5 +1,5 @@
-import chatgpt from "./chatgpt.svg"
-import user from "./user.png"
+import chatgpt from './chatgpt.svg'
+import user from './user.png'
 
 function Conversation(props) {
   return (
@@ -8,24 +8,24 @@ function Conversation(props) {
         item.user === 'user' ? (
           <div className="w-full flex flex-row-reverse mt-2 mb-2" key={index}>
             <div className="basis-1/6 flex justify-center">
-							<img src={user} alt="" className="h-10 w-10" />
-						</div>
-            <div className="basis-4/6">
-							<div className="bg-gray-200 p-2 rounded-tl-xl  rounded-br-xl rounded-bl-xl shadow-md -mr-1">
-								{item.content}
-							</div>
-						</div>
+              <img src={user} alt="" className="h-10 w-10" />
+            </div>
+            <div className="basis-4/6 flex flex-row-reverse">
+              <div className="bg-gray-200 p-2 rounded-tl-xl  rounded-br-xl rounded-bl-xl shadow-md -mr-1">
+                <p style={{ wordWrap: 'break-word', wordBreak: 'break-all' }}>{item.content}</p>
+              </div>
+            </div>
           </div>
         ) : (
           <div className="w-full flex flex-row mt-2 mb-2" key={index}>
             <div className="basis-1/6 flex justify-center">
-							<img src={chatgpt} alt="" className="h-10 w-10" />
-					  </div>
-            <div className="basis-4/6">
-							<div className="bg-red-200 p-2 rounded-br-xl  rounded-tr-xl rounded-bl-xl shadow-md -ml-1">
-								{item.content}
-							</div>
-						</div>
+              <img src={chatgpt} alt="" className="h-10 w-10" />
+            </div>
+            <div className="basis-4/6 flex">
+              <div className="bg-red-200 p-2 rounded-br-xl  rounded-tr-xl rounded-bl-xl shadow-md -ml-1">
+                <p style={{ wordWrap: 'break-word', wordBreak: 'break-all' }}>{item.content}</p>
+              </div>
+            </div>
           </div>
         )
       )}
